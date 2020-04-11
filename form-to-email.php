112192d22f33885648e873d30255e6a24c0b22b1
David Campbell <davidthecomposer@gmail.com>
2020-04-11 15:05:47 -0600

    updated email handlers

--- form-to-email.php
+++ form-to-email.php
@@ -1,9 +1,4 @@
 <?php
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
-}
 $name = $_POST['user-name'];
 $visitor_email = $_POST['user-email'];
 $message = $_POST['email-content'];
