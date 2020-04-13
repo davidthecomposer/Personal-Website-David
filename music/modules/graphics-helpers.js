85bf2af888dd3bc5f7d9e2083ea5538c1f77dce4
David Campbell <davidthecomposer@gmail.com>
2020-04-13 13:04:37 -0600

    updated folder structure and made splash page

---
+++ music/modules/graphics-helpers.js
@@ -0,0 +1,16 @@
+ /* jshint esversion: 6 */
+
+export const underlineGraphics = () => {
+
+    const nav = document.querySelector('nav');
+    const pageNav = nav.querySelectorAll('.header-tab');
+ 
+    const handleTabLeave = (e) => {
+      e.target.nextElementSibling.classList.add('retract');
+    };
+ 
+    pageNav.forEach((btn) => {
+      btn.addEventListener('mouseout', handleTabLeave);
+    });
+  };
+ 
