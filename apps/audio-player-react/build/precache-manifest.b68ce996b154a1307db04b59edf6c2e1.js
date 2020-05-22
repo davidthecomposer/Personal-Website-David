5850d03426fcab1e0038df4b812cc499fd29406b
David Campbell <davidthecomposer@gmail.com>
2020-05-22 16:34:47 -0600

    updated build

---
+++ apps/audio-player-react/build/precache-manifest.b68ce996b154a1307db04b59edf6c2e1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ada20f668ec0c3a61f95ed680dbb77f",
+    "url": "/audio-player-react/build/index.html"
+  },
+  {
+    "revision": "1676ca441a854124eede",
+    "url": "/audio-player-react/build/static/css/main.94e54951.chunk.css"
+  },
+  {
+    "revision": "57908cb16ed9423f2386",
+    "url": "/audio-player-react/build/static/js/2.d243cb20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/audio-player-react/build/static/js/2.d243cb20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1676ca441a854124eede",
+    "url": "/audio-player-react/build/static/js/main.cb619711.chunk.js"
+  },
+  {
+    "revision": "aae193ab0196c0efa571",
+    "url": "/audio-player-react/build/static/js/runtime-main.76b764a8.js"
+  },
+  {
+    "revision": "30b560286cc33e79544cd4905cc84f47",
+    "url": "/audio-player-react/build/static/media/Forgiveness.30b56028.jpg"
+  },
+  {
+    "revision": "6b993eb2eb175e11892821efec9a2ef2",
+    "url": "/audio-player-react/build/static/media/Music for a Love Story.6b993eb2.wav"
+  },
+  {
+    "revision": "dd57a75ff46c4f95162992762cded584",
+    "url": "/audio-player-react/build/static/media/Turning Point.dd57a75f.wav"
+  },
+  {
+    "revision": "e72c601624d50bab46ca4d2efa1f9ed4",
+    "url": "/audio-player-react/build/static/media/pause_button.e72c6016.svg"
+  },
+  {
+    "revision": "192e9085415dffa4eecec8cb18c6d672",
+    "url": "/audio-player-react/build/static/media/play_button.192e9085.svg"
+  },
+  {
+    "revision": "5ff1be29df22195d7ab92e0a8e9009e2",
+    "url": "/audio-player-react/build/static/media/turningpoint.5ff1be29.jpg"
+  },
+  {
+    "revision": "385dc5778e8c31eca18e23c5b895da7d",
+    "url": "/audio-player-react/build/static/media/volumeIcon.385dc577.svg"
+  },
+  {
+    "revision": "244e62e4f0dc1c26538a88310340a4c6",
+    "url": "/audio-player-react/build/static/media/volumeMute.244e62e4.svg"
+  }
+]);
