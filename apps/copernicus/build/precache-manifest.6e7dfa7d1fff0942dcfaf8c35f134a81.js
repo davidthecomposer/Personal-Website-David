9d174d15fb8945df7d95121d960da597da29c1eb
David Campbell <davidthecomposer@gmail.com>
2020-08-15 14:02:57 -0600

    added build folder

---
+++ apps/copernicus/build/precache-manifest.6e7dfa7d1fff0942dcfaf8c35f134a81.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362c9ff53568f8368dbdbbd344406fe2",
+    "url": "/copernicus/build/index.html"
+  },
+  {
+    "revision": "1eff475b4fc6e804b518",
+    "url": "/copernicus/build/static/css/main.2fa8fcff.chunk.css"
+  },
+  {
+    "revision": "f256d5330d975ecf1e7b",
+    "url": "/copernicus/build/static/js/2.d23ad33a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/copernicus/build/static/js/2.d23ad33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eff475b4fc6e804b518",
+    "url": "/copernicus/build/static/js/main.72616cde.chunk.js"
+  },
+  {
+    "revision": "e0b3f689e7f35f7e0703",
+    "url": "/copernicus/build/static/js/runtime-main.d8f34b7c.js"
+  },
+  {
+    "revision": "17663dc40be71c2a2386f0d56c3ce5d8",
+    "url": "/copernicus/build/static/media/Earth.17663dc4.wav"
+  },
+  {
+    "revision": "6236af164bdce5edc3d0eec70e2a3564",
+    "url": "/copernicus/build/static/media/Jupiter.6236af16.wav"
+  },
+  {
+    "revision": "186d45562749fd58b78b512d297a6309",
+    "url": "/copernicus/build/static/media/Mars.186d4556.wav"
+  },
+  {
+    "revision": "366fc173af7676cead25ff43d2cabd04",
+    "url": "/copernicus/build/static/media/Mercury.366fc173.wav"
+  },
+  {
+    "revision": "574f62ccc7ac3dbe8408c9cc00c737ef",
+    "url": "/copernicus/build/static/media/Neptune.574f62cc.wav"
+  },
+  {
+    "revision": "c51a0b3dea4a700e90203969cb3c32bc",
+    "url": "/copernicus/build/static/media/Neptune.c51a0b3d.svg"
+  },
+  {
+    "revision": "59d8da51ee49d0106e043bb1d21ec77a",
+    "url": "/copernicus/build/static/media/Neptune_orbit.59d8da51.svg"
+  },
+  {
+    "revision": "e1c3dd2b33f5f32053dda9f0e29c2d11",
+    "url": "/copernicus/build/static/media/Pluto.e1c3dd2b.wav"
+  },
+  {
+    "revision": "ff475143faab8a690a370cf7e647aa3e",
+    "url": "/copernicus/build/static/media/Saturn.ff475143.wav"
+  },
+  {
+    "revision": "d29359be6a26c493fcbc3e19d8522a4d",
+    "url": "/copernicus/build/static/media/Uranus.d29359be.wav"
+  },
+  {
+    "revision": "4e69ea88fb9baa360b47de0d3fbbc64d",
+    "url": "/copernicus/build/static/media/Venus.4e69ea88.mp3"
+  },
+  {
+    "revision": "08d67a3a9254f70c003f7efc208ee52b",
+    "url": "/copernicus/build/static/media/background_4X.08d67a3a.jpeg"
+  },
+  {
+    "revision": "4f12e1dfdb76d24377cd0a3b77a91c3a",
+    "url": "/copernicus/build/static/media/comment_icon.4f12e1df.svg"
+  },
+  {
+    "revision": "2abef74007d69d624ccef110af73ec5e",
+    "url": "/copernicus/build/static/media/earth.2abef740.svg"
+  },
+  {
+    "revision": "e350f0b9fcffefd9e6c1e6b25739f4f5",
+    "url": "/copernicus/build/static/media/earth_orbit.e350f0b9.svg"
+  },
+  {
+    "revision": "34246b8fe9e47367e8a308a95dd3833b",
+    "url": "/copernicus/build/static/media/info_button.34246b8f.svg"
+  },
+  {
+    "revision": "49f0b5265ddff6e8bb344b8552067051",
+    "url": "/copernicus/build/static/media/jupiter.49f0b526.svg"
+  },
+  {
+    "revision": "078cf78af06a5b3c4eaaa2a72496f0b7",
+    "url": "/copernicus/build/static/media/jupiter_orbit.078cf78a.svg"
+  },
+  {
+    "revision": "b9f8a719553bf205131632a5f86e71bf",
+    "url": "/copernicus/build/static/media/mars.b9f8a719.svg"
+  },
+  {
+    "revision": "bc69a3b80be1a40614473d71766e6439",
+    "url": "/copernicus/build/static/media/mars_orbit.bc69a3b8.svg"
+  },
+  {
+    "revision": "49830bd426776309a981db8e08689f55",
+    "url": "/copernicus/build/static/media/mercury.49830bd4.svg"
+  },
+  {
+    "revision": "961ef8669f18c348af2bcc2993e58f19",
+    "url": "/copernicus/build/static/media/mercury_orbit.961ef866.svg"
+  },
+  {
+    "revision": "daf5fc43ef1a545aed59c65f3f909b54",
+    "url": "/copernicus/build/static/media/nvm.daf5fc43.svg"
+  },
+  {
+    "revision": "7bf4f11c588773e8a3d8ed11b8033a14",
+    "url": "/copernicus/build/static/media/pause_button.7bf4f11c.svg"
+  },
+  {
+    "revision": "8de15c15a27fe56bf6e3f3067962d253",
+    "url": "/copernicus/build/static/media/play_button.8de15c15.svg"
+  },
+  {
+    "revision": "8d5841678d9866c044bd31df5f31048f",
+    "url": "/copernicus/build/static/media/pluto.8d584167.svg"
+  },
+  {
+    "revision": "c15853f55dcac3040f9367edc846d2be",
+    "url": "/copernicus/build/static/media/pluto_orbit.c15853f5.svg"
+  },
+  {
+    "revision": "2f7e62045505084a41ad50c73e1f45b0",
+    "url": "/copernicus/build/static/media/saturn.2f7e6204.svg"
+  },
+  {
+    "revision": "e1eef26a35ec7a39fedd24433606fddd",
+    "url": "/copernicus/build/static/media/saturn_orbit.e1eef26a.svg"
+  },
+  {
+    "revision": "abafdb3782ef77c3fc0f3769d8cb3355",
+    "url": "/copernicus/build/static/media/slide_in.abafdb37.svg"
+  },
+  {
+    "revision": "73160ebeba174796c4c6acd58b4a8cf6",
+    "url": "/copernicus/build/static/media/slide_out.73160ebe.svg"
+  },
+  {
+    "revision": "ae4226065b0b35161d8d86494f4aa86e",
+    "url": "/copernicus/build/static/media/sun_orbit.ae422606.svg"
+  },
+  {
+    "revision": "72e14b6a6c3fb8c4cbc098250b67eaa2",
+    "url": "/copernicus/build/static/media/switch-off.72e14b6a.svg"
+  },
+  {
+    "revision": "7f4fe62a5ad9f9f4110e49c079dc17fa",
+    "url": "/copernicus/build/static/media/switch_on.7f4fe62a.svg"
+  },
+  {
+    "revision": "fc60eb3f403f050d9fa91ec70a8dd640",
+    "url": "/copernicus/build/static/media/uranus.fc60eb3f.svg"
+  },
+  {
+    "revision": "d775d220275322081e3ef95786bd9752",
+    "url": "/copernicus/build/static/media/uranus_orbit.d775d220.svg"
+  },
+  {
+    "revision": "b17cbc33bad67fdb1aaafebf301d0dca",
+    "url": "/copernicus/build/static/media/venus.b17cbc33.svg"
+  },
+  {
+    "revision": "0792aba46f52ccfd5a75edf0537ebf2b",
+    "url": "/copernicus/build/static/media/venus_orbit.0792aba4.svg"
+  }
+]);
