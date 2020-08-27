8c2e7ca38041eeb23b697d35e84a6b41c98a1376
David Campbell <davidthecomposer@gmail.com>
2020-08-27 12:58:42 -0600

    added build files

---
+++ dev-blog/build/precache-manifest.c01c52876d992257a1e64a5849806655.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa79f78b200f616a8a77d4f21ed2e64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3f200e8870a903d780b",
+    "url": "/static/css/main.6f5064b3.chunk.css"
+  },
+  {
+    "revision": "4cb8728ebee6a56115c7",
+    "url": "/static/js/2.f2495b34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2495b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f200e8870a903d780b",
+    "url": "/static/js/main.262cdf24.chunk.js"
+  },
+  {
+    "revision": "f099465289ec46bd8e81",
+    "url": "/static/js/runtime-main.99a0bdbb.js"
+  },
+  {
+    "revision": "28a2289967d4454ae74bf7de61c1e736",
+    "url": "/static/media/David_Smile.28a22899.png"
+  },
+  {
+    "revision": "7c93b65f43c85ce2f1899e1b720915c0",
+    "url": "/static/media/after.7c93b65f.png"
+  },
+  {
+    "revision": "fa2f588313488224003ce3e0dbf21ed7",
+    "url": "/static/media/flatline.fa2f5883.png"
+  },
+  {
+    "revision": "770303788ca731b838f151b6240bb678",
+    "url": "/static/media/love-story.77030378.png"
+  },
+  {
+    "revision": "2d54c66665140cdedf59ab5448e8dca1",
+    "url": "/static/media/rescue.2d54c666.png"
+  }
+]);
