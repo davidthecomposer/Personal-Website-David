9019d475b33ad474703128cafa899e0191f02df3
David Campbell <davidthecomposer@gmail.com>
2020-09-12 13:08:25 -0600

    edited author picture

--- dev-blog/src/Components/Main/Articles/Author.jsx
+++ dev-blog/src/Components/Main/Articles/Author.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import davidImage from "../../../Images/ShareLinks/David_Smile.webp";
+import davidImageBU from "../../../Images/ShareLinks/David_Smile.jpeg";
 import "../../../Styles/Author.scss";
 
 const Author = ({ date }) => {
 	return (
 		<div className='author'>
 			<p className='article-date'>{date}</p>
-			<img className='author-image' src={davidImage} alt='' />
+			<picture>
+				<source srcSet={davidImageBU} />
+				<source srcSet={davidImage} />
+				<img className='author-image' src={davidImage} alt='' />
+			</picture>
+
 			<p className='author-name'>David Campbell</p>
 		</div>
 	);
